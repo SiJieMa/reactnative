7bcedb83af101093d9f793f12231230b7dfe1f21
SiJieMa <940825279@qq.com>
2018-05-13 23:58:41 +0800

    new statrt

--- App.js
+++ App.js
@@ -4,66 +4,66 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import TabNavigator from 'react-native-tab-navigator';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
+    Platform,
+    StyleSheet,
+    Text,
+    View
 } from 'react-native';
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
+    ios: 'Press Cmd+R to reload,\n' +
     'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
+    android: 'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
 
 type Props = {};
 export default class App extends Component<Props> {
-  render() {
-    return (
+    render() {
+        return (
 
-        <TabNavigator>
-    <TabNavigator.Item
-      selected={this.state.selectedTab === 'home'}
-      title="Home"
-      renderIcon={() => <Image source={...} />}
-      renderSelectedIcon={() => <Image source={...} />}
-      badgeText="1"
-      onPress={() => this.setState({ selectedTab: 'home' })}>
-      {homeView}
-    </TabNavigator.Item>
-    <TabNavigator.Item
-      selected={this.state.selectedTab === 'profile'}
-      title="Profile"
-      renderIcon={() => <Image source={...} />}
-      renderSelectedIcon={() => <Image source={...} />}
-      renderBadge={() => <CustomBadgeView />}
-      onPress={() => this.setState({ selectedTab: 'profile' })}>
-      {profileView}
-    </TabNavigator.Item>
-  </TabNavigator>
-    );
-  }
+            <TabNavigator>
+                <TabNavigator.Item
+                    selected={this.state.selectedTab === 'home'}
+                    title="Home"
+                    renderIcon={() => <Image source={...}/>}
+                    renderSelectedIcon={() => <Image source={...}/>}
+                    badgeText="1"
+                    onPress={() => this.setState({selectedTab: 'home'})}>
+                    {homeView}
+                </TabNavigator.Item>
+                <TabNavigator.Item
+                    selected={this.state.selectedTab === 'profile'}
+                    title="Profile"
+                    renderIcon={() => <Image source={...}/>}
+                    renderSelectedIcon={() => <Image source={...}/>}
+                    renderBadge={() => <CustomBadgeView/>}
+                    onPress={() => this.setState({selectedTab: 'profile'})}>
+                    {profileView}
+                </TabNavigator.Item>
+            </TabNavigator>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
